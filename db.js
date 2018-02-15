7f0a3fd78f7a7379097cb5e3c5ad10490ec824cf
Adil Aliyev <adilaliev@gmail.com>
2018-02-15 20:47:56 +0900

    Introduce main.js as a controller

--- db.js
+++ db.js
@@ -2,6 +2,9 @@
  * @author Adil Aliyev <adilaliev@gmail.com>
  */
 
+'use strict';
+/* jshint esversion: 6*/
+
 class DB {
     constructor() {
         this.db = window.localStorage;
--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 /**
- * Created by adil on 18. 2. 15.
+ * @author Adil Aliyev <adilaliev@gmail.com>
  */
+
+'use strict';
+/* jshint esversion: 6*/
+
+let db = new DB();
+
+new Menu(function (data) {
+    let newItem = {};
+    newItem.type = data.info.menuItemId;
+    newItem.url = data.tab.url;
+    newItem.text = data.info.selectionText;
+
+    //db.insert(newItem.url, newItem);
+    alert(JSON.stringify(newItem, null, 4));
+
+    // TODO:
+    chrome.browserAction.setBadgeText({text: "1"});
+});
